5505f49c64225b809c33fa30428d843308fe7bb0
Serge M <alotoftype@gmail.com>
2021-01-16 14:38:13 -0500

    adding logo and buttons

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,26 @@
 <template>
   <div class="container">
     <div>
-      <Logo />
+      <img src="~/assets/img/logo.png" alt="Logo" width="50%">
       <h1 class="title">
-        kikisCatering
+        Kiki's Catering
       </h1>
       <div class="links">
         <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
+          href="/"
           target="_blank"
           rel="noopener noreferrer"
           class="button--grey"
         >
-          GitHub
+         Instagram
+        </a>
+        <a
+          href="/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          Contact Us
         </a>
       </div>
     </div>
@@ -40,34 +40,7 @@ export default {}
   align-items: center;
   text-align: center;
 }
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
 .links {
-  padding-top: 15px;
+  padding-top: 1em;
 }
 </style>
